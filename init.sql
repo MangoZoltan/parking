7e0c72b23c49dcc1a00b0273d08dc180d8b87634
MangoZoltan <mangozoltan@gmail.com>
2026-08-05 03:58:04 +0200

    db_inital_data_encoding_fix

--- init.sql
+++ init.sql
@@ -1,27 +1,12 @@
--- phpMyAdmin SQL Dump
--- version 5.2.3
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Aug 04, 2026 at 11:42 PM
--- Server version: 8.4.3
--- PHP Version: 8.3.30
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
---
--- Database: `parking_reservation`
---
-CREATE DATABASE IF NOT EXISTS `parking_reservation` DEFAULT CHARACTER SET utf8mb3 COLLATE utf8mb3_hungarian_ci;
-USE `parking_reservation`;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `reservations`
---
 
 CREATE TABLE `reservations` (
   `id` int NOT NULL,
@@ -30,10 +15,6 @@ CREATE TABLE `reservations` (
   `end_time` datetime NOT NULL,
   `reserver` varchar(255) COLLATE utf8mb3_hungarian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_hungarian_ci;
-
---
--- Dumping data for table `reservations`
---
 
 INSERT INTO `reservations` (`id`, `space`, `start_time`, `end_time`, `reserver`) VALUES
 (1, 'A2', '2026-08-04 18:30:00', '2026-08-04 20:30:00', 'László'),
@@ -44,24 +25,22 @@ INSERT INTO `reservations` (`id`, `space`, `start_time`, `end_time`, `reserver`)
 (6, 'B1', '2026-08-04 19:30:00', '2026-08-04 22:05:00', 'Péter'),
 (7, 'C4', '2026-08-05 10:30:00', '2026-08-05 12:00:00', 'Sára'),
 (8, 'C4', '2026-08-06 16:10:00', '2026-08-07 09:00:00', 'Lajos'),
-(9, 'C4', '2026-08-06 10:30:00', '2026-08-06 12:00:00', 'Sára'),
-(10, 'A5', '2026-08-04 23:59:00', '2026-08-5 07:50:00', 'Marci');
-
---
--- Table structure for table `spaces`
---
+(9, 'C4', '2026-08-06 10:30:00', '2026-08-06 12:00:00', 'Sári'),
+(10, 'A5', '2026-08-04 23:59:00', '2026-08-05 07:50:00', 'Marci');
 
 CREATE TABLE `spaces` (
   `id` int NOT NULL,
   `name` varchar(120) COLLATE utf8mb3_hungarian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_hungarian_ci;
 
---
--- Dumping data for table `spaces`
---
-
 INSERT INTO `spaces` (`id`, `name`) VALUES
 (1, 'A1'),
+(11, 'A10'),
+(12, 'A11'),
+(13, 'A12'),
+(14, 'A13'),
+(15, 'A14'),
+(16, 'A15'),
 (2, 'A2'),
 (3, 'A3'),
 (5, 'A4'),
@@ -70,13 +49,8 @@ INSERT INTO `spaces` (`id`, `name`) VALUES
 (8, 'A7'),
 (9, 'A8'),
 (10, 'A9'),
-(11, 'A10'),
-(12, 'A11'),
-(13, 'A12'),
-(14, 'A13'),
-(15, 'A14'),
-(16, 'A15'),
 (17, 'B1'),
+(26, 'B10'),
 (18, 'B2'),
 (19, 'B3'),
 (20, 'B4'),
@@ -85,35 +59,28 @@ INSERT INTO `spaces` (`id`, `name`) VALUES
 (23, 'B7'),
 (24, 'B8'),
 (25, 'B9'),
-(26, 'B10'),
 (27, 'C1'),
 (28, 'C2'),
 (29, 'C3'),
 (30, 'C4'),
 (31, 'C5');
 
---
--- Indexes for table `reservations`
---
+
 ALTER TABLE `reservations`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `spaces`
---
 ALTER TABLE `spaces`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `name` (`name`);
 
---
--- AUTO_INCREMENT for table `reservations`
---
+
 ALTER TABLE `reservations`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
---
--- AUTO_INCREMENT for table `spaces`
---
 ALTER TABLE `spaces`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
